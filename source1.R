00bb789c0c2ee961da658e0db3e5dd311e97e301
snarles <snarles@stanford.edu>
2016-01-16 10:49:23 -0800

    alignment plot

--- source1.R
+++ source1.R
@@ -1,8 +1,8 @@
 
 
-
+library(rgl)
 library(oro.nifti)
-
+library(prodlim)
 
 get_fi <- function(sub, run, cope) {
   subst <- c("001","002","003","004","005","006","007","008",
@@ -11,3 +11,36 @@ get_fi <- function(sub, run, cope) {
          "task001_run00", run, ".feat/reg_standard/stats/cope",
          cope, ".nii.gz")
 }
+
+# get outer points from an array (done by taking dimensionwise min elts)
+get_outer_points <- function(a) {
+  min_inds <- apply(a, c(1, 2), function(v) min(c(Inf, which(v > 0))))
+  run_inds3a <- cbind(row(min_inds)[min_inds < Inf],
+                    col(min_inds)[min_inds < Inf],
+                    min_inds[min_inds < Inf])
+  min_inds <- apply(a, c(1, 3), function(v) min(c(Inf, which(v > 0))))
+  run_inds2a <- cbind(row(min_inds)[min_inds < Inf],
+                      min_inds[min_inds < Inf],
+                     col(min_inds)[min_inds < Inf])
+  min_inds <- apply(a, c(2, 3), function(v) min(c(Inf, which(v > 0))))
+  run_inds1a <- cbind(min_inds[min_inds < Inf],
+                    row(min_inds)[min_inds < Inf],
+                    col(min_inds)[min_inds < Inf])
+  min_inds <- apply(a, c(1, 2), function(v) max(c(-Inf, which(v > 0))))
+  run_inds3b <- cbind(row(min_inds)[min_inds > -Inf],
+                     col(min_inds)[min_inds > -Inf],
+                     min_inds[min_inds > -Inf])
+  min_inds <- apply(a, c(1, 3), function(v) max(c(-Inf, which(v > 0))))
+  run_inds2b <- cbind(row(min_inds)[min_inds > -Inf],
+                      min_inds[min_inds > -Inf],
+                     col(min_inds)[min_inds > -Inf])
+  min_inds <- apply(a, c(2, 3), function(v) max(c(-Inf, which(v > 0))))
+  run_inds1b <- cbind(min_inds[min_inds > -Inf],
+                     row(min_inds)[min_inds > -Inf],
+                     col(min_inds)[min_inds > -Inf])
+  run_inds <- rbind(run_inds1a, run_inds2a, run_inds3a,
+                    run_inds1b, run_inds2b, run_inds3b)
+  run_inds <- unique(run_inds)
+  return(run_inds)
+}
+
--- temp_script1.R
+++ temp_script1.R
@@ -7,6 +7,10 @@ image(roi_dat)
 ## mask
 roi_mask <- readNIfTI('roi/zstat_mask.nii.gz')
 image(roi_mask)
+mask_inds <- which(roi_mask == 1, arr.ind = TRUE)
+mask_inds <- t(t(mask_inds) - c(27, 32, 23))
+mask_inds <- mask_inds * 3
+plot3d(mask_inds[mask_inds[, 1] < 0, ], aspect = FALSE)
 
 ## Only uncorr results in clusters
 rois <- readNIfTI('roi/thresh_zstat_uncorr.nii.gz')
@@ -20,6 +24,11 @@ rois # 53 x 63 x 46
 ## 16 subjects, 3 runs, 16 copes
 run1_1_1 <- readNIfTI(get_fi(5, 2, 11))
 run1_1_1 # 91 x 109 x 91
+run_inds <- which(run1_1_1 > 0, arr.ind = TRUE)
+run_inds <- t(t(run_inds) - c(46, 55, 46))
+run_inds <- run_inds * 2
+plot3d(run_inds[run_inds[, 1] <= 0, ], aspect = FALSE)
+
 
 orthographic(run1_1_1)
 orthographic(roi_dat)
@@ -52,3 +61,19 @@ image(run1_1_1[16, , ], col = grey.colors(10))
 
 image(run1_1_1[86, , ], col = grey.colors(10))
 image(run1_1_1[6, , ], col = grey.colors(10))
+
+
+#####
+##  ALIGNMENT PLOTS
+#####
+
+mask_inds <- which(roi_mask == 1, arr.ind = TRUE)
+mask_inds <- t(t(mask_inds) - c(27, 32, 23))
+mask_inds <- mask_inds * 3
+
+ex_inds <- get_outer_points(run1_1_1)
+ex_inds <- t(t(ex_inds) - c(46, 55, 46))
+ex_inds <- ex_inds * 2
+
+plot3d(ex_inds[ex_inds[, 1] <= 0, ], aspect = FALSE, size= 3)
+plot3d(mask_inds[mask_inds[, 1] <= 0, ], aspect = FALSE, add = TRUE, col = "green")
