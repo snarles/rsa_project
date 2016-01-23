96e33860aa4f72f520f3d1c14ee2dcbc4c121bd4
snarles <snarles@stanford.edu>
2016-01-23 14:19:31 -0800

    avgd across subjects, linear model best

--- analysis2.R
+++ analysis2.R
@@ -23,45 +23,90 @@ nrois <- max(cls[, "clus"])
 ##  Classifying using models for individual subjects
 ####
 
-residual_offdiag2 <- function (X, Y, B, filt = rep(TRUE, dim(Y)[2]), shrink = 0.5, 
-          mc.cores = 1, ...) 
-{
-  Y <- Y[, filt]
-  Yh <- paramultiply(X, B, mc.cores)
-  resids <- Y - Yh
-  (1 - shrink) * cov(resids) + shrink * diag(diag(cov(resids)) + 1e-5)
-}
+scores_mat <- function(indiv_scale, quad_scale, l2p) {
+  scores <- matrix(1, 16, nrois)
+  
+  for (roi_ind in 1:nrois) {
+    for (sub_ind in 1:16) {
+      roi_dat <- rdat[hdat[, "sub"] == sub_ind, cls[, "clus"] == roi_ind]
+      z_true <- hdat[hdat[, "sub"] == sub_ind, "cope"]
+      params2 <- cbind(scale(params), quad_scale * scale(params^2), 
+                       quad_scale * scale(params[, 1] * params[, 2]),
+                       indiv_scale * eye(16))
+      
+      ## normalize the roi_dat
+      roi_dat <- scale(roi_dat, TRUE, TRUE)
 
-scores <- matrix(1, 16, nrois)
-
-for (roi_ind in 1:nrois) {
-  for (sub_ind in 1:16) {
-    roi_dat <- rdat[hdat[, "sub"] == sub_ind, cls[, "clus"] == roi_ind]
-    z_true <- hdat[hdat[, "sub"] == sub_ind, "cope"]
-    
-    ## strength of regularization
-    indiv_scale <- 10
-    params2 <- cbind(scale(params), indiv_scale * eye(16))
-    
-    ## normalize the roi_dat
-    roi_dat <- scale(roi_dat, TRUE, TRUE)
-    
-    # test (1, 2, 3)
-    te_run <- 1
-    te_inds <- (te_run - 1)* 16 + 1:16
-    X_tr <- repmat(params2, 2, 1)
-    X_te <- params2
-    Y_tr <- roi_dat[-te_inds, ]
-    Y_te <- roi_dat[te_inds, ]
-    
-    res <- identification_pipeline1(X_tr, Y_tr, X_te, Y_te, 1:16,  
-                                    forward_method = fit_ridge_kernel,
-                                    forward_params = list(lambda = 1e-3),
-                                    Sigma_e_method = residual_offdiag,
-                                    Sigma_e_params = list(shrink = 1),
-                                    backward_method = pre_mle,
-                                    scoring_method = resample_misclassification,
-                                    scoring_params = list(m = 2))
-    scores[sub_ind, roi_ind] <- res$score
+      cv_scores <- numeric(3)
+      # test (1, 2, 3)
+      for (te_run in 1:3) {
+        te_inds <- (te_run - 1)* 16 + 1:16
+        X_tr <- repmat(params2, 2, 1)
+        X_te <- params2
+        Y_tr <- roi_dat[-te_inds, ]
+        Y_te <- roi_dat[te_inds, ]
+        
+        res <- identification_pipeline1(X_tr, Y_tr, X_te, Y_te, 1:16,  
+                                        forward_method = fit_ridge_kernel,
+                                        forward_params = list(lambda = l2p),
+                                        Sigma_e_method = residual_offdiag,
+                                        Sigma_e_params = list(shrink = 1),
+                                        backward_method = pre_mle,
+                                        scoring_method = resample_misclassification,
+                                        scoring_params = list(m = 2))
+        cv_scores[te_run] <- res$score
+      }
+      scores[sub_ind, roi_ind] <- mean(cv_scores)
+    }
   }
+  scores
 }
+
+scores <- scores_mat(indiv_scale=1, quad_scale=0, l2p=1e6)
+colMeans(scores)
+mean(colMeans(scores)[1:5])
+mean(colMeans(scores))
+
+####
+##  Average multiple subjects
+####
+
+avgd_scores <- function(indiv_scale, quad_scale, l2p) {
+  av_scores <- numeric(nrois)
+  for (roi_ind in 1:nrois) { 
+    roi_dat <- rdat[, cls[, "clus"] == roi_ind]
+    av_all <- 1/16 * repmat(eye(48), 1, 16) %*% roi_dat
+    dim(roi_dat)
+    dim(av_all)
+    av_all <- scale(av_all, TRUE, TRUE)
+    params2 <- cbind(scale(params), quad_scale * scale(params^2), 
+                     quad_scale * scale(params[, 1] * params[, 2]),
+                     indiv_scale * eye(16))
+    
+    cv_scores <- numeric(3)
+    # test (1, 2, 3)
+    for (te_run in 1:3) {
+      te_inds <- (te_run - 1)* 16 + 1:16
+      X_tr <- repmat(params2, 2, 1)
+      X_te <- params2
+      Y_tr <- av_all[-te_inds, ]
+      Y_te <- av_all[te_inds, ]
+      
+      res <- identification_pipeline1(X_tr, Y_tr, X_te, Y_te, 1:16,  
+                                      forward_method = fit_ridge_kernel,
+                                      forward_params = list(lambda = l2p),
+                                      Sigma_e_method = residual_offdiag,
+                                      Sigma_e_params = list(shrink = 1),
+                                      backward_method = pre_mle,
+                                      scoring_method = resample_misclassification,
+                                      scoring_params = list(m = 2))
+      cv_scores[te_run] <- res$score
+    }
+    av_scores[roi_ind] <- mean(cv_scores)
+  }
+  av_scores
+}
+
+av_scores <- avgd_scores(indiv_scale = 0, quad_scale= 1, l2p= 1e6)
+av_scores
+mean(av_scores)
