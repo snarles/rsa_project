9a1130576595be450b3a98962d7173b84787c91a
Charles Zheng <snarles@stanford.edu>
2016-06-24 18:09:20 -0700

    looked at data

--- birds_analysis/look_at_data.R
+++ birds_analysis/look_at_data.R
@@ -7,8 +7,10 @@ gplot <- function(sl) {
   image(t(flipud(sl)), col = gray.colors(10))
 }
 
-res <- oro.nifti::readNIfTI("DavisPoldrack_Birds_Archive/betaseries_MNI/bg_image.nii.gz")
+res0 <- oro.nifti::readNIfTI("DavisPoldrack_Birds_Archive/betaseries_MNI/bg_image.nii.gz")
 res <- oro.nifti::readNIfTI("DavisPoldrack_Birds_Archive/betaseries_MNI/002_lsone_MNI.nii.gz")
+dim(res0)
 dim(res)
 
+gplot(res0[, , 30])
 gplot(res[, , 30, 100])
