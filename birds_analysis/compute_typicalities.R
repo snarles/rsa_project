a1c16c55e6ae1e1d5357784946d51327a1f78afb
snarles <snarles@stanford.edu>
2016-07-21 19:32:50 -0700

    compute physical typicality

--- birds_analysis/compute_typicalities.R
+++ birds_analysis/compute_typicalities.R
@@ -49,3 +49,47 @@ plot3d(mat)
 ##   11   13      B asymmetric U-shaped in height (greater typ for short)
 ##   12   14      C wonton: more U-shaped in angle
 ##   13   17      B increasing linearly in angle, u-shaped in height
+
+
+####
+## COMPUTE PHYSICAL TYPICALITIES
+####
+
+allp <- do.call(rbind, paramz)
+apply(allp, 2, range)
+
+## angle from -25 to 65
+## height from 57.5 to 207.5
+
+angs <- c(-25, 65)
+heits <- c(57.5, 207.5)
+angRANGE <- max(angs) - min(angs)
+heitRANGE <- max(heits) - min(heits)
+angl_h <- mean(angs)
+heit_h <- mean(heits)
+umix <- c(0.25, 0.75)
+lmix <- c(0.75, 0.25)
+
+mdptz <- list()
+ptypz <- list()
+for (i in 1:13) {
+  params <- paramz[[i]]
+  nest <- 1 + (params[, 1] > angl_h) + 2 * (params[, 2] > heit_h)
+  mida <- ifelse(params[, 1] > angl_h, sum(umix * angs), sum(lmix * angs))
+  midh <- ifelse(params[, 2] > heit_h, sum(umix * heits), sum(lmix * heits))
+  mdpts <- cbind(nest, mida, midh)
+  mdptz[[i]] <- mdpts
+  diFF <- params[, -3] - mdpts[, -1]
+  diFF[, 1] <- diFF[, 1]/angRANGE
+  diFF[, 2] <- diFF[, 2]/heitRANGE
+  ptyps <- cbind(diFF, rowSums(diFF^2), exp(-10 * rowSums(diFF^2)))
+  ptypz[[i]] <- ptyps
+  # plot(params[, -3])
+  # for (i in 1:4) {
+  #   points(params[nest==i, -3], col = rainbow(4)[i])
+  #   points(mida[nest==i] + 2 * rnorm(sum(nest==i)), midh[nest==i] + 2 * rnorm(sum(nest==i)), 
+  #          col = rainbow(4)[i], pch = "+")
+  # }
+}
+
+save(mdptz, ptypz, paramz, stypz, file = "birds_analysis/computed_typs.RData")
