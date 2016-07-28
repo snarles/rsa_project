5a549443c2a707cc99573c50c9604c2f596d674e
snarles <snarles@stanford.edu>
2016-07-28 09:37:08 -0700

    varying lambda

--- birds_analysis/whole_brain_typicality.R
+++ birds_analysis/whole_brain_typicality.R
@@ -3,21 +3,23 @@ source("birds_analysis/dataloading.R")
 
 ind <- 3
 brain <- get_brain(ind)
-lbda1 <- 10
-lbda2 <- 10
-phtyp <- get_typicality("ph", lbda1, ind)
-pstyp <- get_typicality("ps", lbda2, ind)
+lbdas <- c(2,5,10)
+nl <- length(lbdas)
 
+mat_sub <- matrix(nl, 3)
 t1 <- proc.time()
-m_0 <- projection_brainmap(brain, rep(1, length(phtyp)))
-m_ph <- projection_brainmap(brain, cbind(1, phtyp))
-m_ps <- projection_brainmap(brain, cbind(1, pstyp))
-m_both <- projection_brainmap(brain, cbind(1, pstyp, phtyp))
+
+for (i in 1:nl) {
+  lbda1 <- lbdas[i]
+  lbda2 <- lbdas[i]
+  phtyp <- get_typicality("ph", lbda1, ind)
+  pstyp <- get_typicality("ps", lbda2, ind)
+  m_ph <- projection_brainmap(brain, cbind(1, phtyp))
+  m_ps <- projection_brainmap(brain, cbind(1, pstyp))
+  m_both <- projection_brainmap(brain, cbind(1, pstyp, phtyp))
+  mat_sub[i, ] <- c(sum(m_ph), sum(m_ps), sum(m_both))
+}
 proc.time() - t1
 
-f2(m_0)
 
-
-dim(m_0)
-
-gplot(m_0[, , 40])
+cbind(lbdas, mat_sub)
