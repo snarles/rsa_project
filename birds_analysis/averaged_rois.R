55ba5141559cf1ce2f8d8efdbd148f39c7e97322
snarles <snarles@stanford.edu>
2016-08-03 16:07:26 -0700

    average-then-cluster results are quite different

--- birds_analysis/averaged_rois.R
+++ birds_analysis/averaged_rois.R
@@ -29,34 +29,76 @@ save(m_allz, file = "birds_analysis/m_allz.rda")
 m_allz <- lapply(m_allz, log)
 
 
-infl_point_lvs <- function(v) {
-  v <- sort(m_allz[[1]])
-  v <- v[v > -Inf]
-  plot(v, type = "l")
-  dv <- v[-1] - v[-length(v)]
-  plot(dv, type = "l", ylim = c(-1e-2, 1e-2))
-  d2v <- dv[-1] - dv[-length(dv)]
-  plot(d2v, type = "l", ylim = c(-1e-3, 1e-3))
-  abline(h = 1e-4, col = "red")
-  
+ma_filt_v <- function(v, bandw = 100) {
+  trunc_len <- floor(length(v)/bandw) * bandw
+  v_trunc <- v[1:trunc_len]
+  vmat <- matrix(v_trunc, nrow = bandw)
+  vred <- colMeans(vmat)
+  vred
 }
 
-plot(sort(m_allz[[1]]), type = "l", main = 1)
-plot(sort(m_allz[[2]]), type = "l", main = 2)
-plot(sort(m_allz[[7]]), type = "l", main = 7)
-plot(sort(m_allz[[12]]), type = "l", main = 12)
+endpointz <- list()
+for (indx in 1:13) {
+  v0 <- sort(m_allz[[indx]])
+  v <- v0[v0 > 0]
+  v <- ma_filt_v(v, 50)
+  sdv <- sd(v)
+  xx <- 1:length(v)
+  lres <- loess(v ~ xx, span = 0.05, family = "symmetric")
+  #plot(v, type = "l")
+  #lines(lres$fitted, col = "red")
+  v <- lres$fitted
+  dv <- v[-1] - v[-length(v)]
+  xx <- 1:length(dv)
+  lres2 <- loess(dv ~ xx, span = 0.1, family = "symmetric")
+  #plot(dv, type = "l", ylim = c(-1e-2, 1e-2))
+  #lines(lres2$fitted, type = "l", ylim = c(-1e-2, 1e-2), col = "red")
+  dv <- lres2$fitted
+  d2v <- dv[-1] - dv[-length(dv)]
+  # plot(d2v, type = "l", ylim = c(-1e-4, 1e-4))
+  # abline(h = 4e-6, col = "red"); abline(h = -4e-6, col = "red")
+  inds <- which(abs(d2v) < 1e-5)  
+  endpoints <- range(v[inds + 1])
+  plot(v0, type = "l")
+  abline(h = endpoints[1], col = "red")
+  abline(h = endpoints[2], col = "red")
+  title(indx)
+  endpointz[[indx]] <- endpoints
+}
+thresholds <- sapply(endpointz, `[[`, 2)
 
+# plot(sort(m_allz[[1]]), type = "l", main = 1)
+# plot(sort(m_allz[[2]]), type = "l", main = 2)
+# plot(sort(m_allz[[7]]), type = "l", main = 7)
+# plot(sort(m_allz[[12]]), type = "l", main = 12)
+# 
+# 
+# hist(m_allz[[1]], breaks = 30)
+# plot3d(which(m_allz[[1]] > 17, arr.ind = TRUE))
+# #points3d(which(m_allz[[2]] > 16, arr.ind = TRUE), col = "red")
+# #points3d(which(m_allz[[4]] > 17, arr.ind = TRUE), col = "yellow")
+# points3d(which(m_allz[[5]] > 18, arr.ind = TRUE), col = "yellow")
+# 
+# 
+# plot3d(which(m_allz[[2]] > 17, arr.ind = TRUE), col = "red")
+# #points3d(which(m_allz[[1]] > 17, arr.ind = TRUE))
+# #points3d(which(m_allz[[3]] > 17, arr.ind = TRUE), col = "blue")
+# points3d(which(m_allz[[4]] > 17, arr.ind = TRUE), col = "yellow")
 
-hist(m_allz[[1]], breaks = 30)
-plot3d(which(m_allz[[1]] > 17, arr.ind = TRUE))
-#points3d(which(m_allz[[2]] > 16, arr.ind = TRUE), col = "red")
-#points3d(which(m_allz[[4]] > 17, arr.ind = TRUE), col = "yellow")
-points3d(which(m_allz[[5]] > 18, arr.ind = TRUE), col = "yellow")
+indx <- 5; plot3d(which(m_allz[[indx]] > thresholds[indx], TRUE), col = "black")
+indx <- 8; points3d(which(m_allz[[indx]] > thresholds[indx], TRUE), col = "red")
 
+m_avg <- Reduce(`+`, m_allz)/13
+dim(m_avg)
+plot(sort(m_avg), type = "l")
+plot3d(which(m_avg > 16, TRUE))
+indx <- 1; points3d(which(m_allz[[indx]] > thresholds[indx], TRUE), col = "blue")
+indx <- 3; points3d(which(m_allz[[indx]] > thresholds[indx], TRUE), col = "green")
+indx <- 5; points3d(which(m_allz[[indx]] > thresholds[indx], TRUE), col = "red")
+indx <- 7; points3d(which(m_allz[[indx]] > thresholds[indx], TRUE), col = "yellow")
 
-plot3d(which(m_allz[[2]] > 17, arr.ind = TRUE), col = "red")
-#points3d(which(m_allz[[1]] > 17, arr.ind = TRUE))
-#points3d(which(m_allz[[3]] > 17, arr.ind = TRUE), col = "blue")
-points3d(which(m_allz[[4]] > 17, arr.ind = TRUE), col = "yellow")
-
-
+plot3d(which(m_avg > 16, TRUE))
+indx <- 9; points3d(which(m_allz[[indx]] > thresholds[indx], TRUE), col = "blue")
+indx <- 10; points3d(which(m_allz[[indx]] > thresholds[indx], TRUE), col = "green")
+indx <- 12; points3d(which(m_allz[[indx]] > thresholds[indx], TRUE), col = "red")
+indx <- 13; points3d(which(m_allz[[indx]] > thresholds[indx], TRUE), col = "yellow")
